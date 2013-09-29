27d960a4ad9ab6b5e4b87437713650b65b62d0c9
Benjamin Winkler <abwinkler999@gmail.com>
2013-09-28 22:08:50 -0400

    Can read in a single file entry

--- lib/ocr_kata_ruby.rb
+++ lib/ocr_kata_ruby.rb
@@ -1,10 +1,12 @@
 class Check_Set
+	attr_accessor :check_file
+
 	def initialize
+		@check_file = []
 	end
 
-end
-
-class Entry
-	attr_accessor :text, :digits
+	def read_checks(check_file_path)
+		@check_file = IO.readlines(check_file_path)
+	end
 
 end
--- spec/ocr_kata_ruby_spec.rb
+++ spec/ocr_kata_ruby_spec.rb
@@ -2,12 +2,13 @@ require 'ocr_kata_ruby'
 
 describe Check_Set do
 	before(:each) do
-		check_file = File.new("lib/checks.txt")
+		@check_file_path = File.new("lib/checks.txt")
 	end
 
-	it "can read an entry from a text file" do
-		subject.read_checks(check_file)
-		subject.check_data.length.should == 108 # four lines of 27 characters each
+	it "can read an entry from a text file containing one check" do
+		subject.read_checks(@check_file_path)
+		subject.check_file.length.should == 4
 	end
+	
 
 end
