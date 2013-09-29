97ad40451589b3da132dbd2acd911ee4d7951593
Benjamin Winkler <abwinkler999@gmail.com>
2013-09-29 01:04:15 -0400

    Moving on to isolating individual digits

--- lib/ocr_kata_ruby.rb
+++ lib/ocr_kata_ruby.rb
@@ -1,3 +1,12 @@
+class Check
+	attr_accessor :digits, :lines
+
+	def initialize
+		@digits = []
+		@lines = []
+	end
+end
+
 class Check_Set
 	attr_accessor :check_file_lines, :checks
 
@@ -10,7 +19,9 @@ class Check_Set
 		@check_file_lines = IO.readlines(check_file_path)
 	end
 
-	def subdivide_checks
+	def subdivide_into_checks
+		@checks = @check_file_lines.each_slice(4).to_a
+		# this subdivides into 11 arrays of 4 lines each
 	end
 
 end
