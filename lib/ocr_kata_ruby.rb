234326dc65da68babd3e2fef143db294bcff880b
Benjamin Winkler <abwinkler999@gmail.com>
2013-09-28 21:56:04 -0400

    Created first test

---
+++ lib/ocr_kata_ruby.rb
@@ -0,0 +1,10 @@
+class Check_Set
+	def initialize
+	end
+
+end
+
+class Entry
+	attr_accessor :text, :digits
+
+end
--- spec/ocr_kata_ruby_spec.rb
+++ spec/ocr_kata_ruby_spec.rb
@@ -1,1 +1,13 @@
 require 'ocr_kata_ruby'
+
+describe Check_Set do
+	before(:each) do
+		check_file = File.new("lib/checks.txt")
+	end
+
+	it "can read an entry from a text file" do
+		subject.read_checks(check_file)
+		subject.check_data.length.should == 108 # four lines of 27 characters each
+	end
+
+end
