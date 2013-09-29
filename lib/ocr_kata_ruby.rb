90e2f840f3bf0b2cd07a014dfaaa36d52754a79b
Benjamin Winkler <abwinkler999@gmail.com>
2013-09-28 23:11:00 -0400

    Created proper source data file; minor test refactor

--- lib/ocr_kata_ruby.rb
+++ lib/ocr_kata_ruby.rb
@@ -1,12 +1,16 @@
 class Check_Set
-	attr_accessor :check_file
+	attr_accessor :check_file_lines, :checks
 
 	def initialize
-		@check_file = []
+		@check_file_lines = []
+		@checks = []
 	end
 
 	def read_checks(check_file_path)
-		@check_file = IO.readlines(check_file_path)
+		@check_file_lines = IO.readlines(check_file_path)
+	end
+
+	def subdivide_checks
 	end
 
 end
