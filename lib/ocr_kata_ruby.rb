fd9c85a0623bc45f78aef02d31f5d261785243a0
Benjamin Winkler <abwinkler999@gmail.com>
2013-09-30 23:00:22 -0400

    Refactored digit reader

--- lib/ocr_kata_ruby.rb
+++ lib/ocr_kata_ruby.rb
@@ -1,64 +1,65 @@
 class Check
-	attr_accessor :raw_digits, :digits, :lines
+  attr_accessor :raw_digits, :digits, :lines
 
-	def initialize
-		@raw_digits = []
-		@digits = []
-		@lines = []
-	end
+  def initialize
+    @raw_digits = []
+    @digits = []
+    @lines = []
+  end
 end
 
-class Check_Set
-	attr_accessor :check_file_lines, :checks
+class Check_File
+  attr_accessor :check_file_lines, :entries
 
-	def initialize
-		@check_file_lines = []
-		@checks = []
-	end
+  def initialize
+    @check_file_lines = []
+    @entries = []
+  end
 
-	def read_checks(check_file_path)
-		@check_file_lines = IO.readlines(check_file_path)
-	end
+  def read_checks(check_file_path)
+    @check_file_lines = IO.readlines(check_file_path)
+  end
 
-	def subdivide_file_into_checks
-		@check_file_lines.each_slice(4).to_a.each { |x| 
-			temp_check = Check.new
-			temp_check.lines = x
-			[0, 3, 6, 9, 12, 15, 18, 21, 24].each { |digit_pos|
-				temp_check.raw_digits << [x[0][digit_pos, 3], x[1][digit_pos, 3], x[2][digit_pos, 3]].join
-				temp_check.digits << match_digit([x[0][digit_pos, 3], x[1][digit_pos, 3], x[2][digit_pos, 3]].join)
-			}
-			# there's got to be a way to dry this out
-			@checks << temp_check
-		}
-	end
+  def subdivide_file_into_checks
+    @check_file_lines.each_slice(4).to_a.each { |x| 
+      temp_check = Check.new
+      temp_check.lines = x
+      #puts {"hi "+ "there"}
+      for digit_number in 0..8
+        line_position = digit_number * 3
+        temp_check.raw_digits << [x[0][line_position, 3], x[1][line_position, 3], x[2][line_position, 3]].join
+        temp_check.digits << match_digit([x[0][line_position, 3], x[1][line_position, 3], x[2][line_position, 3]].join)
+      end
+      @entries << temp_check
+    }
+  end
 
 
-	def match_digit(candidate)
-		case candidate
-		when " _ | ||_|"
-			return 0
-		when "     |  |"
-			return 1
-		when " _  _||_ "
-			return 2
-		when " _  _| _|"
-			return 3
-		when "   |_|  |"
-			return 4
-		when " _ |_  _|"
-			return 5
-		when " _ |_ |_|"
-			return 6
-		when " _   |  |"
-			return 7
-		when " _ |_||_|"
-			return 8
-		when " _ |_| _|"
-			return 9
-		else
-			return -1
-		end
-	end
+  def match_digit(candidate)
+    case candidate
+    when " _ | ||_|"
+      return 0
+    when "     |  |"
+      return 1
+    when " _  _||_ "
+      return 2
+    when " _  _| _|"
+      return 3
+    when "   |_|  |"
+      return 4
+    when " _ |_  _|"
+      return 5
+    when " _ |_ |_|"
+      return 6
+    when " _   |  |"
+      return 7
+    when " _ |_||_|"
+      return 8
+    when " _ |_| _|"
+      return 9
+    else
+      return -1
+    end
+  end
 
 end
--- spec/ocr_kata_ruby_spec.rb
+++ spec/ocr_kata_ruby_spec.rb
@@ -1,33 +1,32 @@
 require 'ocr_kata_ruby'
 
-describe Check_Set do
-	before(:each) do
-		@check_file_path = File.new("lib/checks.txt")
-		subject.read_checks(@check_file_path)
-		subject.subdivide_file_into_checks
-	end
+describe Check_File do
+  before(:each) do
+    @check_file_path = File.new("lib/checks.txt")
+    subject.read_checks(@check_file_path)
+    subject.subdivide_file_into_checks
+  end
 
-	it "can read in check entries from a text file" do
-		subject.check_file_lines.length.should == 44
-	end
+  it "can read in check entries from a text file" do
+    subject.check_file_lines.length.should == 44
+  end
 
-	it "can subdivide a text file into separate checks" do
-		subject.checks.length.should == 11
-	end
+  it "can subdivide a text file into separate checks" do
+    subject.entries.length.should == 11
+  end
 
-	it "can subdivide a check into nine separate digits" do
-		subject.checks[0].raw_digits[0].should == " _ | ||_|"
-		subject.checks[0].raw_digits.length.should == 9
-	end
+  it "can subdivide a check into nine separate digits" do
+    subject.entries[0].raw_digits[0].should == " _ | ||_|"
+    subject.entries[0].raw_digits.length.should == 9
 
-
-	it "can identify a digit under ideal conditions" do
-		subject.checks[0].digits[0].should == 0
-	end
-
-	it "can OCR an entire check under ideal conditions" do
-		subject.checks[10].digits.join.should == "123456789"
-	end
+  end
 
 
+  it "can identify a digit under ideal conditions" do
+    subject.entries[0].digits[0].should == 0
+  end
+
+  it "can OCR an entire check under ideal conditions" do
+    subject.entries[10].digits.join.should == "123456789"
+  end
 end
